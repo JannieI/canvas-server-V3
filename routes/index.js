27bb60d30d06d1dd3c3c6da1f8e2c9b6befdf1f0
JannieI <jimmelman@gmail.com>
2018-11-26 19:08:39 +0200

    Add id route

--- routes/index.js
+++ routes/index.js
@@ -29,14 +29,14 @@ router.get('/', function(req, res, next) {
 });
 
 
-// Get canvasBackgroundcolorsDefaults
+// Get ALL / SELECTED canvasBackgroundcolorsDefaults
 router.get('/background', (req, res, next) => {
     // Get the page nr to start from the query string
     let pageStartNrRequested = req.query.page;
     let pageSize = 19;    // Is actually 20 since we base 0
     if (pageStartNrRequested == undefined) {
         pageStartNrRequested = 1;   // Make default 1 => rows start at 0
-        pageSize = canvasBackgroundcolorsDefaults.length;
+        pageSize = canvasBackgroundcolorsDefaults.length - 1;
     };
 
     // Define starting position, and extract required data
@@ -44,7 +44,33 @@ router.get('/background', (req, res, next) => {
     const results = canvasBackgroundcolorsDefaults.slice(rowStartNr, rowStartNr + pageSize);
 
     // Return
-    res.json(results);
+    res.json( 
+        { 
+            page: pageStartNrRequested,
+            rows: pageSize,
+            results: results
+        }
+    );
+});
+
+// Get SINGLE canvasBackgroundcolorsDefaults
+router.get('/background:id', (req, res, next) => {
+    // Get the page nr to start from the query string
+
+    const id = req.params.id;
+    // Define starting position, and extract required data
+    const results = canvasBackgroundcolorsDefaults.filter(row => {
+        row.id == Number(id);
+    });
+
+    // Return
+    res.json( 
+        { 
+            id: id,
+            rows: 1,
+            results: results
+        }
+    );
 });
 
 module.exports = router;
