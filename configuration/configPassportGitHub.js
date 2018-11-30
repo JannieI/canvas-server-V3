2caf190dcd8565e230b81b44545e8087e6887ad5
JannieI <jimmelman@gmail.com>
2018-11-30 21:00:58 +0200

    Fake config

--- configuration/configPassportGitHub.js
+++ configuration/configPassportGitHub.js
@@ -1,7 +1,7 @@
 // GitHub OAuth config
 
 module.exports = {
-    clientID: '803643b025d3ff9553e3',
-    clientSecret: '8783f9266836e76125fa2b5c13d32007360803ff',
-    callbackURL: "http://localhost:8000/auth/github/callback"
+    clientID: '',
+    clientSecret: '',
+    callbackURL: ""
 }
