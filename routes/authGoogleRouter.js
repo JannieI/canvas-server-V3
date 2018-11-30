b73111b3d642f582c36e1755356ee7e3af472e48
JannieI <jimmelman@gmail.com>
2018-11-30 18:54:00 +0200

    Add authGoogleRouter

---
+++ routes/authGoogleRouter.js
@@ -0,0 +1,72 @@
+// Basic imports -----------------------------------------------------------------
+var express = require('express');
+var authGoogleRouter = express.Router();
+
+// Third Party modules -----------------------------------------------------------
+const passport = require('passport');
+const session = require('express-session');
+var GoogleStrategy = require('passport-passport-google-oauth20').Strategy;
+const passportConfig = require('../configuration/configPassportGoogle');
+
+
+// Functions ---------------------------------------------------------------------
+
+
+// Runs for ALL routes ----------------------------------------------------------
+
+// Setup Session for Passport
+authGoogleRouter.use(session({
+    secret: 'I love Canvas!',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+// Passport related 
+authGoogleRouter.use(passport.initialize());
+authGoogleRouter.use(passport.session());
+passport.use(new GoogleStrategy(passportConfig,
+  (accessToken, refreshToken, profile, cb) => {
+    console.log(profile)
+    return cb(null, profile);
+  }
+));
+
+passport.serializeUser( (user, cb)=>{
+    cb(null,user);
+});
+
+passport.deserializeUser((user,cb)=>{
+    cb(null,user)
+});
+
+
+// Methods for this Router -----------------------------------------------------------
+
+// authGoogle/login
+authGoogleRouter.get('/login', passport.authenticate('google'));
+
+authGoogleRouter.get('/callback', passport.authenticate('google',{
+    successRedirect: '/',
+    failureRedirect: '/loginFailed'
+}));
+
+authGoogleRouter.get('/loginSuccess', (req, res, next) => {
+    res.json( {msg: 'Login worked !'} );
+});
+
+authGoogleRouter.get('/loginFailed', (req, res, next) => {
+    res.json( {msg: 'Login failed'} );
+});
+
+// authGoogle/logout
+authGoogleRouter.get('/logout', (req, res, next) => {
+    req.logout();
+    req.session.destroy( () => {
+        res.clearCookie('connect.sid');
+        res.json( {msg: "Logged out"} );
+    });
+});
+
+
+// Export -----------------------------------------------------------
+module.exports = authGoogleRouter;
