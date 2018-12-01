d940b0b007f04a56c25a485c9580659ad680ac52
JannieI <jimmelman@gmail.com>
2018-12-01 06:42:27 +0200

    Config git rm --cached filename

--- configuration/configPassportGoogle.js
+++
@@ -1,7 +0,0 @@
-// Google OAuth config
-// https://console.developers.google.com/apis/credentials My Project 60471
-module.exports = {
-    clientID: '',
-    clientSecret: '',
-    callbackURL: ""
-}
