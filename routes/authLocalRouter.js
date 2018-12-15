27d659b7632dd9c9a0b9d0a74701be32c7d84a2a
JannieI <jimmelman@gmail.com>
2018-12-15 17:51:15 +0200

    signup

--- routes/authLocalRouter.js
+++ routes/authLocalRouter.js
@@ -2,21 +2,11 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-
 const router = express.Router();
 
+const UserModel = require('../model/models');
 
-// router.get('/verify', (req, res, next) => {
-
-//     console.log('GET /verify', req.body, req.params, req.query)
-//     console.log('')
-
-//     if (req.query.userID == 'JannieI') {
-//         res.send(true);
-//     } else {
-//         res.send(false);
-//     }
-// });
+// Verify User as valid (exists in Canvas DB)
 router.post('/verify', (req, res, next) => {
     console.log('Router: POST /verify', req.body)
     console.log('')
@@ -32,16 +22,69 @@ router.post('/verify', (req, res, next) => {
 //middleware created previously
 // curl -v -X POST http://localhost:8000/signup -H "application/json" -d 'password=jannie' -d 'email=jannie@gmail.com'
 // router.post('/signup', passport.authenticate('signup', { session : false }) , async (req, res, next) => {
-router.post('/signup', passport.authenticate('signup', { session : false }) , async (req, res, next) => {
+router.post('/signup', (req, res, next) => {
     console.log('Router: POST signup', req.body.companyName, req.body.userID, req.body.password)
     console.log('')
 
-    // Add to MongoDB
-    // {
-    //     "companyName": givenCompanyName,
-    //     "userID": givenUserID,
-    //     "password": givenPassword 
-    // } 
+    try {
+        console.log('    try block starts')
+        console.log('');
+
+        // Determine if user already exists in the database
+        UserModel.find( { companyName: req.body.companyName, userID: req.body.userID }, 
+            (err, user) => {
+            if (err) {
+
+                // Create a new user record since it does not exist
+                var newUser = UserModel({
+                    companyName: req.body.companyName,
+                    userID: username,
+                    email: username + '@clarityanalytics.xyz',
+                    password: req.body.password,
+                    createdBy: '',
+                    createdOn: null,
+                    updatedBy: '',
+                    updatedOn: null
+
+                });
+
+                // save the user
+                newUser.save((err) => {
+                    if (err) {
+                        console.log('    signup failed: ', err);
+                        return err;
+                    };
+
+                    console.log('    User created!');
+                }); 
+
+                //Send the user information to the next middleware
+                console.log('    auth.signup: Success for ', newUser);
+                res.json({ 
+                    "message" : "Signup successful !",
+                    "user": newUser
+                });
+
+            } else {;            
+                // User found
+                console.log('    auth.signup: User Already exists ', user);
+                res.json({ 
+                    "message" : "User already exists for this Company"
+                });
+            };
+        });
+
+        // //Send the user information to the next middleware
+        // console.log('    auth.signup: Success for ', newUser);
+        // return done(null, newUser);;
+    } catch (error) {
+        console.log('    Error: ', error)
+        res.json({ 
+            "message" : "Error: " ,
+            error: error
+        });
+};
+
 
     res.json({ 
         "message" : "Signup successful !",
@@ -54,6 +97,8 @@ router.post('/signup', passport.authenticate('signup', { session : false }) , as
 router.post('/login', (req, res, next) => {
     console.log('/login (authenticate)');
     console.log('')
+
+    // Do the login via Passport
     passport.authenticate('login', (err, user, info) => {     
         try 
             {
