9e1e82cb8bb1c83a9ad703ac424b58b76ae5d2bd
JannieI <jimmelman@gmail.com>
2018-12-20 13:15:27 +0200

    canvasdata: get param and query

--- routes/canvasDataRouter.js
+++ routes/canvasDataRouter.js
@@ -6,9 +6,11 @@ const router = express.Router();
 const UserModel = require('../model/models');
 
 // Verify User as valid (exists in Canvas DB)
-router.get('/', (req, res, next) => {
+router.get('/:resource', (req, res, next) => {
     console.log('Router: GET ')
     console.log('')
+
+    console.log('xx ', req.query, req.params)
 
     res.send( 
         {
