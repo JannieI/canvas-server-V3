2622b008674b12723454542d6d03f3a90d70f5cb
JannieI <jimmelman@gmail.com>
2018-12-21 10:47:05 +0200

    Minor Changes

--- database/mongoDatabase.js
+++ database/mongoDatabase.js
@@ -1,7 +1,17 @@
+// Database object for Mongo
+
+// Notes from https://medium.freecodecamp.org/introduction-to-mongoose-for-mongodb-d2a7aa593c57:
+//
+// The require(‘mongoose’) call above returns a Singleton object. It means that the first time 
+// you call require(‘mongoose’), it is creating an instance of the Mongoose class and returning it. 
+// On subsequent calls, it will return the same instance that was created and returned to you the 
+// first time because of how module import/export works in ES6.
+
+
 let mongoose = require('mongoose');
-const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
-const database = 'fcc-Mail';      // REPLACE WITH YOUR DB NAME
-class Database {
+const server = '127.0.0.1:27017'; 
+const database = 'Canvas';      
+class MongoDatabase {
   constructor() {
     this._connect()
   }
@@ -15,4 +25,5 @@ _connect() {
        })
   }
 }
-module.exports = new Database()
+
+module.exports = new MongoDatabase()
