c9e13c6fdb7cb0c27568aff6cdbad02027a9604e
JannieI <jimmelman@gmail.com>
2018-12-23 09:38:24 +0200

    canvasUsers

--- model/canvasUsers.js
+++ model/canvasUsers.js
@@ -71,11 +71,45 @@ const CanvasUserSchema = new Schema({
     createdBy: String,                                      // UserID who created this task, can be System
     createdOn: {                                            // Date task was created
         type: Date,
-        // `Date.now()` returns the current unix timestamp as a number
         default: Date.now
-      }
+    }
 });
 
+//This is called a pre-hook, before the user information is saved in the database
+//this function will be called, we'll get the plain text password, hash it and store it.
+CanvasUserSchema.pre('save', async function(next){
+    //'this' refers to the current document about to be saved
+    const user = this;
+    //Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
+    //your application becomes.
+    const hash = await bcrypt.hash(this.password, 10);
+    //Replace the plain text password with the hash and then store it
+    this.password = hash;
+
+
+    // Get the current date
+    var currentDate = new Date();
+
+    // Change the updated_at field to current date
+    this.updatedOn = currentDate;
+
+    // If created_at doesn't exist, add to that field
+    if (!this.createdOn) {
+        this.createdOn = currentDate;
+    };
+
+    //Indicates we're done and moves on to the next middleware
+    next();
+});
+
+//We'll use this later on to make sure that the user trying to log in has the correct credentials
+CanvasUserSchema.methods.isValidPassword = async function(password){
+    const user = this;
+    //Hashes the password sent by the user for login and checks if the hashed password stored in the 
+    //database matches the one sent. Returns true if it does else false.
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+}
 
 // Create Model: modelName, schema, collection
 const CanvasUserModel = mongoose.model('canvasUsers', CanvasUserSchema, 'canvasUsers');
