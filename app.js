3f27368f63074b5fd4ad14528ca126a08f6795a9
JannieI <jimmelman@gmail.com>
2018-12-23 16:51:13 +0200

    Cors PUT

--- app.js
+++ app.js
@@ -78,6 +78,7 @@ app.use( (req, res, next) => {
     // debugDev('Inside CORS');
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
 
