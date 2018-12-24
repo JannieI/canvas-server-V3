b9d09b438fcb9ddcece9f2eeac72d816c16e8756
JannieI <jimmelman@gmail.com>
2018-12-24 07:42:36 +0200

    Add canvasBackgroundcolors

---
+++ model/canvasBackgroundcolors.js
@@ -0,0 +1,27 @@
+// Model for CanvasGroups collection
+
+// Imports
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+// Schema
+const CanvasBackgroundcolorSchema = new Schema({
+    id: Number,                             // Unique Backgroundcolor ID
+    name: String,                           // Backgroundcolor name
+    editedBy: String,                       // Last user who edited this task
+    editedOn: Date,                         // Date this task was last edited
+    createdBy: String,                      // UserID who created this task, can be System
+    createdOn: {                            // Date task was created
+        type: Date,
+        // `Date.now()` returns the current unix timestamp as a number
+        default: Date.now
+      }
+});
+
+
+// Create Model: modelName, schema, collection
+const CanvasBackgroundcolorModel = mongoose.model(
+    'canvasBackgroundcolors', CanvasBackgroundcolorSchema, 'canvasBackgroundcolors');
+
+// Export
+module.exports = CanvasBackgroundcolorModel;
