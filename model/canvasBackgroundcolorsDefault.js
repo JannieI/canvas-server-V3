e8f6ce1ddd31452b887586393f2fc712e2b02101
JannieI <jimmelman@gmail.com>
2018-12-25 08:07:32 +0200

    Add canvasBackgroundcolorsDefault schema

---
+++ model/canvasBackgroundcolorsDefault.js
@@ -0,0 +1,24 @@
+// Model for CanvasGroups collection
+
+// Imports
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+// Schema
+const CanvasBackgroundcolorsDefaultSchema = new Schema({
+    id: Number,                             // Unique record ID
+    name: String,                           // Name, ie brown
+    cssCode: String,                        // CSS code, as name, hex, rgb.  ie transparent, rgb(111,52,78)
+    shortList: Boolean,                     // True if part of shorter list
+});
+
+
+// Create Model: modelName, schema, collection
+const CanvasBackgroundcolorsDefaultModel = mongoose.model(
+    'canvasBackgroundcolorsDefault', 
+    CanvasBackgroundcolorsDefaultSchema, 
+    'canvasBackgroundcolorsDefault'
+);
+
+// Export
+module.exports = CanvasBackgroundcolorsDefaultModel;
