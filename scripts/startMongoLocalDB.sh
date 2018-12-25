2f8b8aaeda32c0b818ad3cb579e1e17639295453
JannieI <jimmelman@gmail.com>
2018-12-25 07:28:36 +0200

    mlc

--- scripts/startMongoLocalDB.sh
+++ scripts/startMongoLocalDB.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Start Mongo Client for cloud mongo server ..."
-alias mls='echo "Mongo LOCAL Server on  --dbpath ~/Projects/canvas-mongoDB"     &&  sudo mong$
-alias mlc='echo "Mongo LOCAL Client"        &&  mongo'
-alias msc='echo "Mongo Client on Cloud Server with JannieI ..."  &&  mongo "mongodb+srv://clu$
-
+echo "Start Mongo Client on Local mongo server ..."
+echo "Mongo LOCAL Client"
+mongo
