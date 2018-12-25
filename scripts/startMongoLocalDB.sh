249d5f66ce06c7bef8cfae41c0a90c756dca7d3d
JannieI <jimmelman@gmail.com>
2018-12-25 07:37:48 +0200

    mlc

--- scripts/startMongoLocalDB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Start Mongo Client on Local mongo server ..."
-echo "Mongo LOCAL Client"
-mongo
---
+++ scripts/startMongoLocalServer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Mongo LOCAL Server on  --dbpath ~/Projects/canvas-mongoDB"
+sudo mongod --dbpath ~/Projects/canvas-mongoDB
+
