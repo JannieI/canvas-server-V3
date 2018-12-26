536b73308503a1c4a3906c6a381e9b6b96407b62
JannieI <jimmelman@gmail.com>
2018-12-26 10:06:25 +0200

    canvasBackgroundcolours

--- model/canvasBackgroundcolors.js
+++ model/canvasBackgroundcolors.js
@@ -3,7 +3,6 @@
 // Imports
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
-const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 // Schema - corresponds to CSScolor model on Workstation
 const CanvasBackgroundcolorSchema = new Schema({
@@ -12,9 +11,6 @@ const CanvasBackgroundcolorSchema = new Schema({
     cssCode: String,                        // CSS code, as name, hex, rgb.  ie transparent, rgb(111,52,78)
     shortList: Boolean,                     // True if part of shorter list
 });
-
-// Auto-Incement the id field
-CanvasBackgroundcolorSchema.plugin(AutoIncrement, {inc_field: 'id'});
 
 // Create Model: modelName, schema, collection
 const CanvasBackgroundcolorModel = mongoose.model(
