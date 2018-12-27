259a5a16eb45c77770d9b750f920aac600c9cdb2
JannieI <jimmelman@gmail.com>
2018-12-27 09:21:28 +0200

    canvasBackgroundcolorsDefault

--- model/canvasBackgroundcolors.js
+++ model/canvasBackgroundcolors.js
@@ -3,6 +3,7 @@
 // Imports
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+const counterModel = require('./counters')
 
 // Schema - corresponds to CSScolor model on Workstation
 const CanvasBackgroundcolorSchema = new Schema({
@@ -10,6 +11,27 @@ const CanvasBackgroundcolorSchema = new Schema({
     name: String,                           // Name, ie brown
     cssCode: String,                        // CSS code, as name, hex, rgb.  ie transparent, rgb(111,52,78)
     shortList: Boolean,                     // True if part of shorter list
+});
+
+// This pre-hook is called before the information is saved into the database
+CanvasBackgroundcolorSchema.pre('save', function(next) {
+    var doc = this;
+
+    // Find in the counters collection, increment and update
+    counterModel.findOneAndUpdate(
+        {_id: 'canvasBackgroundcolors.id'},
+        {$inc: { seq: 1} },
+        { upsert: true, new: true },
+        function(error, counter)   {
+            if(error) {
+                console.error('Error: findOneAndUpdate in canvasAuditTrails failed.' + error.message);
+                return next(error);
+            };
+
+            doc.id = counter.seq;
+            next();
+        }
+    );
 });
 
 // Create Model: modelName, schema, collection
--- model/canvasBackgroundcolorsDefault.js
+++ model/canvasBackgroundcolorsDefault.js
@@ -3,6 +3,7 @@
 // Imports
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+const counterModel = require('./counters')
 
 // Schema
 const CanvasBackgroundcolorsDefaultSchema = new Schema({
@@ -10,6 +11,26 @@ const CanvasBackgroundcolorsDefaultSchema = new Schema({
     name: String,                           // Name, ie brown
     cssCode: String,                        // CSS code, as name, hex, rgb.  ie transparent, rgb(111,52,78)
     shortList: Boolean,                     // True if part of shorter list
+}); 
+
+// This pre-hook is called before the information is saved into the database
+CanvasBackgroundcolorsDefaultSchema.pre('save', function(next) {
+    var doc = this;
+
+    // Find in the counters collection, increment and update
+    counterModel.findOneAndUpdate(
+        {_id: 'canvasBackgroundcolorsDefault.id'},
+        {$inc: { seq: 1} },
+        { upsert: true, new: true },
+        function(error, counter)   {
+            if(error) {
+                return next(error);
+            };
+
+            doc.id = counter.seq;
+            next();
+        }
+    );
 });
 
 // Create Model: modelName, schema, collection
