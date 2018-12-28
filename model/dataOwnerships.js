9201668f3c0bb5a85af2d156f77ee8ddea67c986
JannieI <jimmelman@gmail.com>
2018-12-28 18:36:29 +0200

    Add dataQualityIssues

--- model/dataOwnerships.js
+++ model/dataOwnerships.js
@@ -7,9 +7,21 @@ const counterModel = require('./counters')
 
 // Schema
 const DataOwnershipSchema = new Schema({
-    dateCreated: {                            // Date task was created
+    id: Number,                             // Unique record ID
+    datasourceID: Number,                   // DS for which the ownership is defined
+    userID: String,                         // Short name to identify issue
+    type: String,                           // User-defined ownership role, ie Owner, Steward, etc
+    description: String,                    // Description of the responsibilities
+    updatedBy: String,                      // UserID who last updated the record
+    updatedOn: Date,                        // Last Date when record was updated
+    createdBy: String,                      // UserID who created the record
+    createdOn: {                            // Date task was created
         type: Date,
+        // `Date.now()` returns the current unix timestamp as a Number,        default: Date.now
     },
+
+    // @RunTime
+    datasourceName: String,                 // Name of the linked DS
 });
 
 // This pre-hook is called before the information is saved into the database
---
+++ model/dataQualityIssues.js
@@ -0,0 +1,46 @@
+// Model for dataQualityIssues collection
+
+// Imports
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const counterModel = require('./counters')
+
+// Schema
+const DataQualityIssueSchema = new Schema({
+    name: String,                           // Short name to identify issue
+    datasourceID: Number,                   // DS to which the issue relates
+    status: String,                         // Status: Logged, InProgress, Solved
+    type: String,                           // User-defined type, ie Stats, Backfill
+    description: String,                    // Description of the issue
+    nrIssues: Number,                       // Optional Nr of issue, can be rounded
+    loggedBy: String,                       // UserID who logged the issue
+    loggedOn: Date,                         // Date Issue was logged
+    solvedBy: String,                       // UserID who solved the issue
+    solvedOn: Date,                         // Date issue was marked as solved
+});
+
+// This pre-hook is called before the information is saved into the database
+DataQualityIssueSchema.pre('save', function(next) {
+    var doc = this;
+
+    // Find in the counters collection, increment and update
+    counterModel.findOneAndUpdate(
+        {_id: 'dataQualityIssues.id'},
+        {$inc: { seq: 1} },
+        { upsert: true, new: true },
+        function(error, counter)   {
+            if(error) {
+                return next(error);
+            };
+
+            doc.id = counter.seq;
+            next();
+        }
+    );
+});
+
+// Create Model: modelName, schema, collection
+const DataQualityIssueModel = mongoose.model('dataQualityIssues', DataQualityIssueSchema, 'dataQualityIssues');
+
+// Export
+module.exports = DataQualityIssueModel;
