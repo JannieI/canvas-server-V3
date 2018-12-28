f63d68f9eb26484132fd668dab1880b7d08f069b
JannieI <jimmelman@gmail.com>
2018-12-28 08:32:30 +0200

    Add dashboardLayouts

--- model/containerStyles.js
+++ model/containerStyles.js
@@ -7,6 +7,7 @@ const counterModel = require('./counters')
 
 // Schema
 const containStyleSchema = new Schema({
+    _id: String,                           // Mongo ID (read only)
     id: Number,                             // Unique ID
     name: String,                           // Unique Name of style
 
