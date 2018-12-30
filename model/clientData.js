543b222d6f6fa3534660d5ad395db782967fb959
JannieI <jimmelman@gmail.com>
2018-12-30 12:15:13 +0200

    Add clientData

---
+++ model/clientData.js
@@ -0,0 +1,38 @@
+// Model for clientData collection
+
+// Imports
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const counterModel = require('./counters')
+
+// Schema
+const ClientDataSchema = new Schema({
+    id: Number,                             // Unique record / document ID
+    data: Object,                           // Data Array (of json objects)
+});
+
+// This pre-hook is called before the information is saved into the database
+ClientDataSchema.pre('save', function(next) {
+    var doc = this;
+
+    // Find in the counters collection, increment and update
+    counterModel.findOneAndUpdate(
+        {_id: 'clientData.id'},
+        {$inc: { seq: 1} },
+        { upsert: true, new: true },
+        function(error, counter)   {
+            if(error) {
+                return next(error);
+            };
+
+            doc.id = counter.seq;
+            next();
+        }
+    );
+});
+
+// Create Model: modelName, schema, collection
+const ClientDataModel = mongoose.model('clientData', ClientDataSchema, 'clientData');
+
+// Export
+module.exports = ClientDataModel;
