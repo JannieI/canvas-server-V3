6f41fd34f68a2ecc64591239803df19c29984017
JannieI <jimmelman@gmail.com>
2019-01-02 19:56:58 +0200

    Comments

--- bin/server.js
+++ bin/server.js
@@ -2,7 +2,7 @@
 var app = require('../app');    // This is where all the routing happens
 var debug = require('debug')('canvas-server:server');
 var http = require('http');
-const socketio = require('socket.io');
+const socketio = require('socket.io');      // Socket Server
 const debugWs = require('debug')('app:ws');
 
 // Get port from environment, else use default
@@ -11,15 +11,65 @@ const port = typeof process.env.SERVER_PORT == 'number'?  process.env.SERVER_POR
 // Create HTTP server
 const server = http.createServer(app);
 
-
 // Socket.io server
 const io = socketio(server);
 
+// Connect event fires whenever a client connects to this server on '/' namespace (the default)
+// socket is the socket connection with the client.  A socket is the fundamental for interacting
+// with browser clients.  It uses underlying Client to communicate.  The socket class inherits
+// from the Node.jse EventEmitter
+
+// Names spaces defines different endpoints or paths to group connections together.
+// It minimizes TCP connections (resources), and introduces separate communication
+// channels.  io.on creates the default (/) namespace.  Each namespace may optionally have a 
+// number of rooms, which clients can join or leave.  io.emit references the default /
+// namespace. It is the same as io.of('/').emit   Similarly, socket. works on /
+// as io.on = io.of('/').on
+// Use const chat = io.of('/chat') to reference the /chat namespace.  And then
+// chat.emit('text') to send a message to all connected clients in the namespace.  Note that
+// namespace.emit does not support acknowlegements / callbacks.  The socket ID for / is the 
+// same as the /chat namespace, just with /chat# prefixed.
+// The server can communicate across different namespaces with io.of('/admin').emit('ns', text'), 
+// but the client socket is defined for // ONE namespace.  One has to define more than one 
+// socket to listen to different name spaces on the client.
 io.on('connect', (socket, req) => {
-    socket.emit('welcome', 'Welcome to web socket server');
+
+    // .emit emits an event to the socket identified by the event name (ie welcome below).  It
+    // can take args (data to be send to the client) and an optional callback which will be called
+    // with the client's answer.
+    // socket.emit() sends a message to one client
+    // socket.to(roomName).emit() sends a messages to all clients in that room, except the sending socket
+    // socket.to(anotherSocketID).emit() sends a message from one socket to another socket
+    // io.of(aNamespace).to(roomName).emit() sends a message to any room
+    // io.emit(), io.of('/').emit(), io.of('/admin').emit() sends a message to the entire namespace
+
+    // socket.emit('welcome', 'Welcome to web socket server');
+    // namespace.to('room').emit() sends to ALL sockets in the room
+    // socket.to('room').emit('eventName', data) sends only to this room, but NOT the socket itself (sender)
+    // socket.join(room, [callback]) - to join a room
+    // socket.leave(room, [callback])
+
+    // ,on registers a new handler for the given event name.  The callback will get whatever data 
+    // was sent over by the client, ie msg below.
     socket.on('message', (msg) => {
         debugWs(msg);
     });
+
+    // Standard event names
+    socket.on('disconnecting', (reason) => {
+        // fires prior to client disconnecting (but hasnot left rooms yet)
+    });
+
+    socket.on('disconnect', (reason) => {
+        // fires when a client disconnected
+        // socket.leave(room, [callback])
+        // Rooms are left automatically on disconnect
+    });
+
+    socket.on('error', (err) => {
+        // fires when an error occured
+    });
+
 });
 
 
