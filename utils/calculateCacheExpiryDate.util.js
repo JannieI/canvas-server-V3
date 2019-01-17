08d7c93b7cb33ad3a98bb1e947ab26fcbfb90506
JannieI <jimmelman@gmail.com>
2019-01-17 17:58:39 +0200

    calculateCacheExpiryDate

--- utils/calculateCacheExpiryDate.util.js
+++ utils/calculateCacheExpiryDate.util.js
@@ -1,18 +1,14 @@
+const dateAdd = require('../utils/dateAdd.util');
+
 module.exports = function calculateCacheExpiryDate(datasource) {
     // This routines calculates the expiry date in cache for a given Datasource
 
     let serverExpiryDateTime = new Date();
-    "oldnessMaxPeriodInterval" : "hour",
-    "oldnessMaxPeriodUnits" : 24,
-    "oldnessRelatedDate" : "",
-    "oldnessRelatedTime" : "",
+    serverExpiryDateTime = dataAdd(
+        datasource.oldnessMaxPeriodInterval, 
+        datasource,oldnessMaxPeriodUnits
+    );
 
-    let tn = dn.getTime()
-    let dl = new Date(dateTime);
-    let tl = dl.getTime();
-    if (tl >= tn) {
-        return true;
-    } else {
-        return serverExpiryDateTime;
-    };
+    console.log('xx serverExpiryDateTime', typeof serverExpiryDateTime, serverExpiryDateTime)
+    return serverExpiryDateTime;
 }
