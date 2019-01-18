821176bab51830ccbc9d9d969ee73a71bea4a638
JannieI <jimmelman@gmail.com>
2019-01-18 19:27:03 +0200

    dataasource.model

--- models/datasources.model.js
+++ models/datasources.model.js
@@ -18,67 +18,98 @@ const DatasourceFilter = new Schema({
     isActive: Boolean,                      // True if activated, else not used
 })
 
+const DatasourceCombinationSpec = new Schema({
+    lefthandDatasourceID: Number,           // Joins LH to RH, identification of LH Datasource
+    lefthandDatasourceField: String,        // LH Datasource field name
+    joinType: String,                       // LeftJoin, InnerJoin, RightJoin, Union, Minus, Intersection, etc
+    righthandDatasourceID: Number,          // Joins LH to RH, identification of RH Datasource
+    righthandDatasourceField: String,       // RH Datasource field name
+
+})
+
+const MetaDataFields = new Schema({
+    fieldName: String,                      // Name of the field
+    fieldAlias: String,                     // Optional Alias (ie SELECT TNX as Transaction) 
+    fieldType: String,                      // Type: string, number, boolean, Array
+    length: String,                         // Maximum length of the field
+    average: String,                        // Optional stats: average in Field
+    max: String,                            // Optional stats: Maximum in Field
+    median: String,                         // Optional stats: Median in Field
+    min: String,                            // Optional stats: Minimum value in Field
+    sum: String,                            // Optional stats: Sum of all values (ie SQL SUM() )
+})
+
+
 // Schema
 const DatasourceSchema = new Schema({
+
+    // WHO
+
+    // Who: Descriptive info
     id: Number,                             // Unique record ID
     type: String,                           // Type of source, ie File, Server, Web, Service
     subType: String,                        // Subtype, ie Excel/ CSV for File, PostgreSQL/ Mongo for Server
     typeVersion: String,                    // Version of source, ie Excel 2016
     name: String,                           // Name of Datasource
-    username: String,                       // Username to log into server (if not via AD)
-    password: String,                       // Password to log into server
     description: String,                    // Description of the DS
-    dataFieldIDs: [ { type: Number } ],     // IDs of fields in DB table
-    dataFields: [ { type: String } ],       // FieldNames, in order to display
-    dataFieldTypes: [ { type: String } ],   // Field Types, same order as dataFields
-    dataFieldLengths: [ { type: Number } ], // Max field lengths, same order as dataFields
 
-	// Parameters and Filters
-    datasourceFilters: [ DatasourceFilter ],    // Optional Array of DS-Filters per Dashboard
-    datasourceFilterForThisDashboard: Boolean,  // @ RunTime, changes: true if THIS D has filters on THIS DS
-
-    // Access Type
+    //  Who: Access Type
     accessType: String,                     // How to access D: Private, Public, AccessList
 
-    // Caching info
-    cacheResultsOnServer: Boolean,          // True if results may be cached on server. Each Tr is decided separately
-    serverExpiryDateTime: Date,             // When cache expires on server
-    unRefreshable: Boolean,                 // Can create once, but cannot Refresh after that
-    cacheResultsLocal: Boolean,             // True if Local results must be cached
+    //  Who: Create and Edit info
+    createMethod: String,                   // Method how DS was created, ie DirectFile, ..., ManagedSQLEditor
+    editor: String,                         // Last Edited By
+    dateEdited: Date,                       // Last Edited On
+    createdBy: String,                      // Creator
+    createdOn: {                            // Date task was created
+        type: Date,
+        // `Date.now()` returns the current unix timestamp as a Number,        default: Date.now
+    },
 
-    // Optional Max Oldness allowed - must be fresher than given
-    oldnessMaxPeriodInterval: String,       // Ie. second, minute, hour, day, month, year
-    oldnessMaxPeriodUnits: Number,          // Must be fresher than say 8 hour
-    oldnessRelatedDate: String,             // Ie. today, yesterday, previousWorkingDay, weekStart, monthStart, yearStart
-    oldnessRelatedTime: String,             // Ie. '08:00' - must be fresher than today 8:00
 
-    // Refresh info
-    refreshedBy: String,                    // Last UserID that refreshed this datasource
-    refreshedServerOn: Date,                // Last dateTime this DS was refreshed on Server
-    refreshedLocalOn: Date,                 // Last dateTime this DS was refreshed locally
+    // WHERE
 
-    // Location and authentication
+    // Where: Optional Combinations (then NO other WHERE info provided)
+    datasourceCombinationSpec: DatasourceCombinationSpec,   // If this DS is a combination of 2 others
+
+    // Where: External Limits
+    rowLimitFromSource: Number,             // Maximum nr rows to return (ie SQL ... LIMIT n), 0 means all
+    timeoutLimitSeconds: Number,            // Timeout (ie for Databases) in seconds, 0 means no limit
+     
+    // Where: External Location for Files
     folder: String,                         // Folder from which the data was loaded
     fileName: String,                       // Filename from which the data was loaded
     excelWorksheet: String,                 // Excel Worksheet name from which the data was loaded
     transposeOnLoad: Boolean,               // True to transpose data before loading (X <-> Y)
     startLineNr: Number,                    // 1 = first = default
+    endLineNr: Number,                      // 2 = second line (NB: base 1).  0 = all lines with data
+    startColumnNr: Number,                  // 1 = first = default
+    endColumnNr: Number,                    // 2 = second line (NB: base 1).  0 = all columns with data
     csvSeparationCharacter: String,         // CSV file column separator: comma or ;
     csvQuotCharacter: String,               // CSV values in "", in '' or without quotes
+    encoding: String,                       // Optional: Ascii, Edcdic (mainframe)
+
+    // Where: External location of web pages
     webUrl: String,                         // URL for web connectors
     webTableIndex: String,                  // Index number (base 0) of table to load, else the Name of the table
 
-    // Managed Connection, Connection created and managed outside of the DS
+    // Where: External location of services
+    serviceUrl: String,                     // URL of REST service
+    serviceParams: String,                  // REST parameters (: Express req object)
+    serviceQueryParams: String,             // REST query parameters (? Express req object)
+    serviceHeaders: String,                 // REST optional headers 
+
+    // Where: Location of Managed Connection, Connection created and managed outside of the DS
     connectionID: Number,                   // Connection to DB
     dataTableID: Number,                    // ID of table linked in DB
-    businessGlossary: String,               // Detailed business oriented description of DS (non-technical)
-    dataDictionary: String,                 // Detailed technical description of DS
 
-    // Direct Connection, all info provided here and once off
-    databaseName: String,                   // DB to connect to
-    port: String,                           // Port on the DB Server
+    // Where: External location for Direct Connection (not using a Managed Connection)
     serverType: String,                     // Server or Host type, ie MySQL, PostgreSQL, etc
     serverName: String,                     // Server or Host name
+    databaseName: String,                   // DB to connect to
+    port: String,                           // Port on the DB Server
+    username: String,                       // Username to log into server (if not via AD)
+    password: String,                       // Password to log into server
     dataTableName: String,                  // Table inside Server with the data
     dataSQLStatement: String,               // SQL Statement to extract data with
     dataNoSQLStatement: String,             // NoSQL Statement to extract data with
@@ -86,18 +117,59 @@ const DatasourceSchema = new Schema({
     dataGraphQLStatement: String,           // GraphQL Statement to extract data with
     dataOverlaySpecification: Object,       // Overlay Specification to extract data with
 
-    // Updated at runtime
+    // Where: Updated at runtime
     nrWidgets: Number,                      // Nr of Ws linked to this DS (at the moment)
 
-    // Create and Edit info
-    editor: String,                         // Last Edited By
-    dateEdited: Date,                       // Last Edited On
-    createMethod: String,                   // Method how DS was created, ie DirectFile, ..., ManagedSQLEditor
-    createdBy: String,                      // Creator
-    createdOn: {                            // Date task was created
-        type: Date,
-        // `Date.now()` returns the current unix timestamp as a Number,        default: Date.now
-    }
+    // Where: External location on client / external Source
+    sourceIsAccessable: Boolean,            // True if Source can be read again.  False for browser uploaded files    
+    
+    // Where: External Parameters (used for the external queries)
+    queryParameters: String,                // SQL Paramters, Mongo Parameters
+    
+    // Where: Internal location on Canvas Server / Caching info
+    // Note: for now, clientData is not cached in Server or Workstation Memory
+    cacheResultsOnServer: Boolean,          // True if results may be cached on server. Each Tr is decided separately
+    serverExpiryDateTime: Date,             // When cache expires on server
+    unRefreshable: Boolean,                 // Can create once, but cannot Refresh after that
+    cacheResultsLocal: Boolean,             // True if Local results must be cached
+
+    // Where: Optional Internal Max Oldness allowed - must be fresher than given
+    oldnessMaxPeriodInterval: String,       // Ie. second, minute, hour, day, month, year
+    oldnessMaxPeriodUnits: Number,          // Must be fresher than say 8 hour
+    oldnessRelatedDate: String,             // Ie. today, yesterday, previousWorkingDay, weekStart, monthStart, yearStart
+    oldnessRelatedTime: String,             // Ie. '08:00' - must be fresher than today 8:00
+
+    // Where: Internal Refresh info
+    refreshedBy: String,                    // Last UserID that refreshed this datasource
+    refreshedServerOn: Date,                // Last dateTime this DS was refreshed on Server
+    refreshedLocalOn: Date,                 // Last dateTime this DS was refreshed locally
+ 
+
+    // WHAT
+
+	// What: Dashboard-applicable Filters
+    datasourceFilters: [ DatasourceFilter ],    // Optional Array of DS-Filters per Dashboard
+    datasourceFilterForThisDashboard: Boolean,  // @ RunTime, changes: true if THIS D has filters on THIS DS
+
+    // What: MetaData (describes the What)
+
+    dataFieldIDs: [ { type: Number } ],     // IDs of fields in DB table
+    dataFields: [ { type: String } ],       // FieldNames, in order to display
+    dataFieldTypes: [ { type: String } ],   // Field Types, same order as dataFields
+    dataFieldLengths: [ { type: Number } ], // Max field lengths, same order as dataFields
+    metaDataField: [ MetaDataFields ],      // Optional: MetaData for Fields (many not be fully populated)
+    businessGlossary: String,               // Detailed business oriented description of DS (non-technical)
+    dataDictionary: String,                 // Detailed technical description of DS
+
+    // What: Transformations (manipulations of the What)
+    transformations: [ DatasourceTransformation ],  // Optional array of transformations done to this Datasource
+
+    // What: Calculated at run-time
+    dataErrorMessage: String,               // Error returned by Canvas Server
+    nrRecordsReturned: Number,              // Nr of records returned by Canvas Server
+    sourceLocation: String,                 // Source, CanvasCacheDisc, CanvasCacheMemory
+
+
 
 });
 
