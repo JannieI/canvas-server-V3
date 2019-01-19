cf733ab82e2b9322654d2b75a4b17a7093be4b32
JannieI <jimmelman@gmail.com>
2019-01-19 14:52:52 +0200

    listFields

--- datalayer/mysql.listFields.datalayer.js
+++ datalayer/mysql.listFields.datalayer.js
@@ -244,6 +244,8 @@ module.exports = function listFields(datasource, queryObject) {
                         "success",
                         "Retrieved data for id: " + datasourceID,
                         results,
+                        null,
+                        null,
                         tableName,
                         nrRecordsReturned,
                         fields
