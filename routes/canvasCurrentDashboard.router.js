c24efb10f2d00b1d569e57eea11582860b3aba3c
JannieI <jimmelman@gmail.com>
2019-01-24 11:02:43 +0200

    Add W model

--- routes/canvasCurrentDashboard.router.js
+++ routes/canvasCurrentDashboard.router.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const debugDev = require('debug')('app:dev');
 const createErrorObject = require('../utils/createErrorObject.util');
 const createReturnObject = require('../utils/createReturnObject.util');
+const dashboardSchema = '../models/dashboards.model';
+const widgetSchema = '../models/widgets.model';
 
 
 // GET route
@@ -19,32 +21,32 @@ router.get('/', (req, res, next) => {
     // Try, in case model file does not exist
     try {
         // Get the model dynamically (take note of file spelling = resource)
-        const canvasSchema = '../models/dashboards.model';
-        const canvasModel = require(canvasSchema);
+        const dashboardModel = require(dashboardSchema);
+        const widgetModel = require(widgetSchema);
 
         // Find the data (using the standard query JSON object)
-        canvasModel.find( query, (err, docs) => {
+        dashboardModel.find( query, (err, dashboards) => {
 
             if (err) {
                 return res.json(createErrorObject(
                     "error",
-                    "Error retrieving Current Dashboard ID: " + req.query.id,
+                    "Error retrieving Dashboard for ID: " + req.query.id,
                     err
-                ))
+                ));
             };
-            
+
             // Return the data with metadata
             return res.json(
                 createReturnObject(
                     "success",
                     "Retrieved data for Current Dashboard ID: " + req.query.id,
-                    docs,
+                    dashboards,
                     null,
                     null,
                     null,
                     null,
                     null,
-                    docs.length,
+                    dashboards.length,
                     null,
                     null
                     )
