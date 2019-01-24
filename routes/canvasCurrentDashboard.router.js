c8be697c85b87e230499bec663c60fee7233ba8d
JannieI <jimmelman@gmail.com>
2019-01-24 10:59:35 +0200

    Add D, v3

--- routes/canvasCurrentDashboard.router.js
+++ routes/canvasCurrentDashboard.router.js
@@ -3,7 +3,6 @@
 // Imports
 const express = require('express');
 const router = express.Router();
-const config = require('config');
 const debugDev = require('debug')('app:dev');
 const createErrorObject = require('../utils/createErrorObject.util');
 const createReturnObject = require('../utils/createReturnObject.util');
@@ -26,15 +25,14 @@ router.get('/', (req, res, next) => {
         // Find the data (using the standard query JSON object)
         canvasModel.find( query, (err, docs) => {
 
-            // KEEP for later !
-            // Extract metodata from the Schema, using one document
-            // const oneDoc = canvasModel.findOne();
-
-
-            // Empty Array of fields
-            var fields = [];
-
-            // console.log('xx COUNT', fields, oneDoc.mongooseCollection.collectionName, docs.length)
+            if (err) {
+                return res.json(createErrorObject(
+                    "error",
+                    "Error retrieving Current Dashboard ID: " + req.query.id,
+                    err
+                ))
+            };
+            
             // Return the data with metadata
             return res.json(
                 createReturnObject(
@@ -47,17 +45,17 @@ router.get('/', (req, res, next) => {
                     null,
                     null,
                     docs.length,
-                    fields,
+                    null,
                     null
                     )
             );
         });
     }
     catch (error) {
-        console.log('WHY ?', error.message)
+        debugDev('Error in canvasCurrentDashboard.router', error.message)
         return res.status(400).json({
             "statusCode": "error",
-            "message" : "No model file for Current Dashboard ID: " + req.query.id,
+            "message" : "Error retrieving Current Dashboard ID: " + req.query.id,
             "data": null,
             "error": error
         });
