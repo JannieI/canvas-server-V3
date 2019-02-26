5acd9e09bfdbad2f61c3f5208f415a31eac10cd6
JannieI <jimmelman@gmail.com>
2019-02-26 06:50:49 +0200

    Dtags to dataCacheTable

--- utils/dataCachingDataMemory.util.js
+++ utils/dataCachingDataMemory.util.js
@@ -6,6 +6,7 @@ var serverMemoryCache =  {
     dashboards: [],               // Corresponds to serverVariableName in dataCachingTable, holds DATA
     datasources: [],
     canvasGroups: [],
+    dashboardTags: [],
     get: function(varName) {
         if (varName == 'dashboards') {
             return serverMemoryCache.dashboards;
@@ -16,8 +17,12 @@ var serverMemoryCache =  {
         if (varName == 'canvasGroups') {
             return serverMemoryCache.canvasGroups;
         };
+        if (varName == 'dashboardTags') {
+            return serverMemoryCache.dashboardTags;
+        };
         return [];
     },
+
     set: function(varName, inputArray) {
         if (varName == 'dashboards') {
             serverMemoryCache.dashboards = inputArray;
@@ -28,7 +33,11 @@ var serverMemoryCache =  {
         if (varName == 'canvasGroups') {
             serverMemoryCache.canvasGroups = inputArray;
         };
+        if (varName == 'dashboardTags') {
+            serverMemoryCache.dashboardTags = inputArray;
+        };
     },
+
     add: function(varName, input) {
         if (varName == 'dashboards') {
             serverMemoryCache.dashboards = serverMemoryCache.dashboards.concat(input);
@@ -39,7 +48,11 @@ var serverMemoryCache =  {
         if (varName == 'canvasGroups') {
             serverMemoryCache.canvasGroups = serverMemoryCache.canvasGroups.concat(input);
         };
+        if (varName == 'dashboardTags') {
+            serverMemoryCache.dashboardTags = serverMemoryCache.dashboardTags.concat(input);
+        };
     },
+
     remove: function(varName, id) {
         if (varName == 'dashboards') {
             serverMemoryCache.dashboards = serverMemoryCache.dashboards.filter(d => d.id != id);
@@ -50,7 +63,11 @@ var serverMemoryCache =  {
         if (varName == 'canvasGroups') {
             serverMemoryCache.canvasGroups = serverMemoryCache.canvasGroups.filter(ds => ds.id != id);
         };
+        if (varName == 'dashboardTags') {
+            serverMemoryCache.dashboardTags = serverMemoryCache.dashboardTags.filter(ds => ds.id != id);
+        };
     },
+
     update: function(varName, id, inputObject) {
         if (varName == 'dashboards') {
             let serverMemoryCacheIndex = serverMemoryCache.dashboards.findIndex(d => d.id == id);
@@ -70,8 +87,13 @@ var serverMemoryCache =  {
                 serverMemoryCache.canvasGroups[serverMemoryCacheIndex] = inputObject;
             };
         };
+        if (varName == 'dashboardTags') {
+            let serverMemoryCacheIndex = serverMemoryCache.dashboardTags.findIndex(d => d.id == id);
+            if (serverMemoryCacheIndex >= 0) {
+                serverMemoryCache.dashboardTags[serverMemoryCacheIndex] = inputObject;
+            };
+        };
     }
-
 };
 
 module.exports.serverMemoryCache = serverMemoryCache;
